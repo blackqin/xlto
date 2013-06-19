d5c58bf549dc6fb7d450039f92da0afe517179fe
blackqin <quiteblack@gmail.com>
2013-06-20 02:11:30 +0800

    XlsToXml
    essential implementation of XlstoXml module.

---
+++ ParseXlsToXml.py
@@ -0,0 +1,32 @@
+import sys
+from XlsToXml import *
+
+# Parse xls files passed as arguments~
+def parseSpecifiedXlsFiles(xlsFiles):
+	parser = XlsToXmlParser()
+	parser.parse(xlsFiles)
+
+# Parse all xls files in current folder~
+def parseAllXlsFiles():
+	print "Error: No xls file accepted!"
+
+
+# Parse xls file(s) from arguments, or all xls files in current folder~
+argvLen = len(sys.argv)
+
+parser = XlsToXmlParser()
+#parser.parseDir("input", "output")
+#parser.parseXls("input/test1.xls", "output")
+parser.parseXlsList(["input/test1.xls", "input/test2.xls"], "output")
+
+"""
+if argvLen > 1:
+	parseSpecifiedXlsFiles(sys.argv[1:])
+else:
+	parseAllXlsFiles()
+"""
+
+"""
+exec test1.xls
+exec test1.xls
+"""
