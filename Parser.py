71d7e9b9ecaab4dfbeb7162b6d96b58f0d697485
Black Qin <quiteblack@gmail.com>
2013-06-20 17:42:08 +0800

    remove comments.

--- Parser.py
+++ Parser.py
@@ -1,7 +1,6 @@
 import sys
 from xlstoxml import *
 
-
 # Parse xls file(s) from arguments~
 argvLen = len(sys.argv)
 
@@ -9,15 +8,3 @@ parser = XlsToXml()
 parser.parseDir("C:/Users/blackqin/Desktop/inputXls", "C:/Users/blackqin/Desktop/outputXml")
 #parser.parseXls("input/test1.xls", "output")
 #parser.parseXlsList(["input/test001.xls", "input/test002.xls"], "output")
-
-"""
-if argvLen > 1:
-	parseSpecifiedXlsFiles(sys.argv[1:])
-else:
-	parseAllXlsFiles()
-"""
-
-"""
-exec test1.xls
-exec test1.xls
-"""
