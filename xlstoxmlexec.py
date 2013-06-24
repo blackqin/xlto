c951fa381a0b1cf7ca5610908407a3de6165ac53
blackqin <quiteblack@gmail.com>
2013-06-25 00:33:40 +0800

    input/output path update
    * clean up xlstoxmlexec.py
    * update input/output path.

--- xlstoxmlexec.py
+++ xlstoxmlexec.py
@@ -5,6 +5,4 @@ from xlstoxml import XlsToXml
 argvLen = len(sys.argv)
 
 parser = XlsToXml()
-parser.parseDir("C:/Users/blackqin/Desktop/inputXls", "C:/Users/blackqin/Desktop/outputXml")
-#parser.parseXls("input/test1.xls", "output")
-#parser.parseXlsList(["input/test001.xls", "input/test002.xls"], "output")
+parser.parseDir("input", "output")
