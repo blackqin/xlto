813ac6cfb1bbe0bd80a308cec0b574c220c020e7
blackqin <quiteblack@gmail.com>
2013-06-25 23:08:44 +0800

    replace tabs with spaces.

--- xlstoxml.py
+++ xlstoxml.py
@@ -10,91 +10,95 @@ from xlto import XlTo
 
 # Excel parser
 class XlsToXml(XlTo):
-	# Variables
-	_reservedRows = 4
-	_nameRowIndex = 2
+    # Variables
+    _reservedRows = 4
+    _nameRowIndex = 2
 
-	# Initialization
-	def __init__(self):
-		pass
+    # Initialization
+    def __init__(self):
+        pass
 
-	@property
-	def reservedRows(self):
-		return self._reservedRows
+    @property
+    def reservedRows(self):
+        return self._reservedRows
 
-	@reservedRows.setter
-	def reservedRows(self, value):
-		self._reservedRows = value
+    @reservedRows.setter
+    def reservedRows(self, value):
+        self._reservedRows = value
 
-	@property
-	def nameRowIndex(self):
-		return self._nameRowIndex
+    @property
+    def nameRowIndex(self):
+        return self._nameRowIndex
 
-	@nameRowIndex.setter
-	def nameRowIndex(self, value):
-		self._nameRowIndex = value
+    @nameRowIndex.setter
+    def nameRowIndex(self, value):
+        self._nameRowIndex = value
 
-	# Parse one xls file~
-	def parseFile(self, filePath, outputDir):
-		xls = xlrd.open_workbook(filePath)
+    # Parse one xls file~
+    def parseFile(self, filePath, outputDir):
+        xls = xlrd.open_workbook(filePath)
 
-		for sheet in xls.sheets():
-			self._parseSheet(sheet, outputDir)
+        for sheet in xls.sheets():
+            self._parseSheet(sheet, outputDir)
 
-	# Parse one sheet~
-	def _parseSheet(self, sheet, outputDir):
-		name = sheet.name
-		rows = sheet.nrows - self.reservedRows
-		cols = sheet.ncols
+    # Parse one sheet~
+    def _parseSheet(self, sheet, outputDir):
+        name = sheet.name
+        rows = sheet.nrows - self.reservedRows
+        cols = sheet.ncols
 
-		# Do nothing if the sheet is empty~
-		if rows <= 0:
-			return
+        # Do nothing if the sheet is empty~
+        if rows <= 0:
+            return
 
-		# Generate xml string~
-		xmlStr = self._toXmlStr(sheet, rows)
+        # Generate xml string~
+        xmlStr = self._toXmlStr(sheet, rows)
 
-		# Save as xml file~
-		fileName = name + ".xml"
-		self._saveFile(outputDir, fileName, xmlStr)
+        # Save as xml file~
+        fileName = name + ".xml"
+        self._saveFile(outputDir, fileName, xmlStr)
 
-	# Convert one sheet to xml string~
-	def _toXmlStr(self, sheet, rows):
-		rootTagName = sheet.name
-		xmlStr = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
-		xmlStr += "<" + rootTagName + ">\n"
+    # Convert one sheet to xml string~
+    def _toXmlStr(self, sheet, rows):
+        rootTagName = sheet.name
+        xmlStr = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
+        xmlStr += "<" + rootTagName + ">\n"
 
-		for i in range(self.reservedRows, self.reservedRows + rows):
-			xmlStr += "\t" + self._toXmlRow(sheet, i) + "\n"
+        for i in range(self.reservedRows, self.reservedRows + rows):
+            xmlStr += "\t" + self._toXmlRow(sheet, i) + "\n"
 
-		xmlStr += "</" + rootTagName + ">"
+        xmlStr += "</" + rootTagName + ">"
 
-		return xmlStr
+        return xmlStr
 
-	# Convert one row to xml string~
-	def _toXmlRow(self, sheet, rowIndex):
-		row = sheet.row(rowIndex)
-		colIndex = 0
-		cellName = ""
-		xmlStr = "<row "
+    # Convert one row to xml string~
+    def _toXmlRow(self, sheet, rowIndex):
+        row = sheet.row(rowIndex)
+        colIndex = 0
+        cellName = ""
+        xmlStr = "<row "
 
-		for cell in row:
-			cellName = (str(sheet.cell_value(self.nameRowIndex, colIndex))).strip()
-			cellValue = (str(self._correctCellValue(cell))).strip()
+        for cell in row:
+            cellName = (str(sheet.cell_value(self.nameRowIndex, colIndex))).strip()
+            cellValue = (str(self._correctCellValue(cell))).strip()
 
-			# Don't add empty cells~
-			if (cellName != ""):
-				xmlStr += cellName + "=\"" + cellValue + "\" "
+            # Don't add empty cells~
+            if (cellName != ""):
+                xmlStr += cellName + "=\"" + cellValue + "\" "
 
-			colIndex += 1
+            colIndex += 1
 
-		xmlStr += "/>"
+        xmlStr += "/>"
 
-		return xmlStr
+        return xmlStr
 
-	# Correct cell value for sure~
-	def _correctCellValue(self, cell):
-		if cell.ctype == xlrd.XL_CELL_NUMBER and cell.value == int(cell.value):
-			return int(cell.value)
-		else:
-			return cell.value
+    # Correct cell value for sure~
+    def _correctCellValue(self, cell):
+        if cell.ctype == xlrd.XL_CELL_NUMBER and cell.value == int(cell.value):
+            return int(cell.value)
+        else:
+            return cell.value
+
+    # Parse one cell~
+    def _parseCell():
+        pass
--- xlto.py
+++ xlto.py
@@ -2,30 +2,30 @@ import os
 
 # Base class to parse Excel file(s) to other expecting file format~
 class XlTo:
-	# Initialization
-	def __init__(self):
-		pass
+    # Initialization
+    def __init__(self):
+        pass
 
-	# Parse all files in "inputDir"
-	def parseDir(self, inputDir, outputDir):
-		for dirPath, dirNames, fileNames in os.walk(inputDir):
-			for fileName in fileNames:
-				filePath = os.path.join(dirPath, fileName)
-				self.parseFile(filePath, outputDir)
+    # Parse all files in "inputDir"
+    def parseDir(self, inputDir, outputDir):
+        for dirPath, dirNames, fileNames in os.walk(inputDir):
+            for fileName in fileNames:
+                filePath = os.path.join(dirPath, fileName)
+                self.parseFile(filePath, outputDir)
 
-	# Parse a list of files~
-	def parseFileList(self, filePathList, outputDir):
-		for filePath in filePathList:
-			self.parseFile(filePath, outputDir)
+    # Parse a list of files~
+    def parseFileList(self, filePathList, outputDir):
+        for filePath in filePathList:
+            self.parseFile(filePath, outputDir)
 
-	# Parse one file~
-	def parseFile(self, filePath, outputDir):
-		print("[XlTo] Virtual method to parse an file~")
+    # Parse one file~
+    def parseFile(self, filePath, outputDir):
+        print("[XlTo] Virtual method to parse an file~")
 
-	# Save as expecting file format~
-	def _saveFile(self, outputDir, fileName, fileContent, fileEncoding="utf-8"):
-		filePath = os.path.join(outputDir, fileName)
+    # Save as expecting file format~
+    def _saveFile(self, outputDir, fileName, fileContent, fileEncoding="utf-8"):
+        filePath = os.path.join(outputDir, fileName)
 
-		file = open(filePath, 'w', encoding=fileEncoding)
-		file.write(fileContent)
-		file.close()
+        file = open(filePath, 'w', encoding=fileEncoding)
+        file.write(fileContent)
+        file.close()
